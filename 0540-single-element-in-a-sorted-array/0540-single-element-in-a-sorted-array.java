a5e6717b439c1effe4033435c3d7c8489dc95fd1
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2026-07-17 10:55:02 +0530

    Time: 0 ms (100%), Space: 52.9 MB (38.95%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -7,36 +7,19 @@ class Solution {
             return nums[0];
         }
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
- 
-            // if mid occurence is starting from odd index that means the single element is on the left side.
-            if (mid - 1 >= start && nums[mid - 1] == nums[mid]) {
-                //starting from an odd occurence;
-                if ((mid - 1) % 2 != 0) {
-                    end = mid - 2;
-                }
-                // starting from a even occurence
-                else {
-                    start = mid + 1;
-                }
+        while (start < end) {
+           int mid = start + (end - start) / 2;
 
-                
-            }
-            // if nums[mid] is repeating on the right.
-             else if (mid + 1 <= end && nums[mid + 1] == nums[mid]) {
-                //if that index is odd, problem is on the left
-                if ((mid) % 2 != 0) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 2;
-                }
-            }
-            else {
-                return nums[mid];
-            }
+           int partnerIndex = mid ^ 1;
+
+           if (nums[mid] == nums[partnerIndex]) {
+                start = mid + 1;
+           } else {
+                end = mid;
+           }
+
         }
 
-        return 0;
+        return nums[start];
     }
 }
