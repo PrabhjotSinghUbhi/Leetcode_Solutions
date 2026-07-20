d00f117ec6842609d5edbbc25e7e3ce2c5d1ccd6
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2026-07-20 22:37:23 +0530

    Time: 37 ms (5.63%), Space: 82.7 MB (28.95%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,75 @@
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        int start = minElement(bloomDay);
+        int end = maxElement(bloomDay);
+        int ans = end;
+
+        if ((long) m * k > bloomDay.length) {
+            return -1;
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            System.out.println(mid);
+            System.out.println(checkIfBloomed(bloomDay, mid, m, k));
+
+            if (checkIfBloomed(bloomDay, mid, m, k)) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    boolean checkIfBloomed(int[] bloomDay, int day, int m, int k) {
+
+        int n = bloomDay.length;
+        int consecutives = 0;
+        int bouquets = 0;
+        for (int i = 0; i < n; i++) {
+
+            if (valid(bloomDay[i],day)) {
+                consecutives++;
+                if(consecutives == k){
+                    bouquets++;
+                    consecutives = 0;
+                }
+            } else {
+                consecutives = 0;
+            }
+
+            if (bouquets >= m){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    boolean valid(int num, int day) {
+        return num <= day;
+    }
+
+    int minElement(int[] nums) {
+        int min = nums[0];
+        for (int i : nums) {
+            if (i < min)
+                min = i;
+        }
+        return min;
+    }
+
+    int maxElement(int[] nums) {
+        int max = nums[0];
+        for (int i : nums) {
+            if (i > max)
+                max = i;
+        }
+        return max;
+    }
+
+}
