f854a7e5efef0fd29cb10dab998e8f489b758df4
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2026-07-21 22:44:19 +0530

    Time: 11 ms (67.64%), Space: 50.4 MB (20.17%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int n = weights.length;
+        int start = maxElement(weights);
+        int end = maxElement(weights) * n;
+        int ans = end;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            boolean bool = checkDays(weights, days, mid);
+
+            // System.out.println(bool + " " + mid);
+
+            if (bool) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+
+    }
+
+    boolean checkDays(int[] nums, int reqDays, int currCapacity) {
+        int daysTaken = 0;
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if(sum == currCapacity) {
+                daysTaken++;
+                sum = 0;
+            } else if (sum > currCapacity) {
+                daysTaken++;
+                sum = 0;
+                i--;
+            }
+
+            if (daysTaken > reqDays) {
+                return false;
+            }
+
+        }
+
+        if(sum != 0) {
+            daysTaken++;
+        }
+
+        return daysTaken <= reqDays;
+    }
+
+    int maxElement(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
