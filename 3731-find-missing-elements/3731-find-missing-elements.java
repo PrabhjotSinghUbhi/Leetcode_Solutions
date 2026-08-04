234f934949c5047819230de704421a749e4217c8
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2026-08-04 09:58:28 +0530

    Time: 4 ms (47.22%), Space: 47 MB (42.16%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,12 +1,17 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        int max = Arrays.stream(nums).max().getAsInt();
-        int min = Arrays.stream(nums).min().getAsInt();
+        // int max = Arrays.stream(nums).max().getAsInt();
+        // int min = Arrays.stream(nums).min().getAsInt();
 
+        int max = maxElement(nums);
+        int min = minElement(nums);
+
+        //n*n
         HashSet<Integer> hs = new HashSet();
         for (int i : nums)
             hs.add(i);
 
+        //n*(maxElement)
         List<Integer> ls = new ArrayList<>();
         for (int i = min; i <= max; i++) {
             if (!hs.contains(i)) {
@@ -15,5 +20,25 @@ class Solution {
         }
 
         return ls;
+    }
+
+    int maxElement(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    int minElement(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
     }
 }
